37c864972920c0c4e38c8e3496e8a9528fb06c2f
valeri <valeriano@gmail.com>
2025-02-19 12:14:37 +0100

    eureka cliente

--- msalumnosprofe/src/main/java/edu/kyndryl/msalumnosprofe/MsalumnosprofeApplication.java
+++ msalumnosprofe/src/main/java/edu/kyndryl/msalumnosprofe/MsalumnosprofeApplication.java
@@ -2,8 +2,10 @@ package edu.kyndryl.msalumnosprofe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient//activamos el cliente de Eureka
 public class MsalumnosprofeApplication {
 
 	public static void main(String[] args) {
